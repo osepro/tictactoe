8b22aae96cd1215c2584d20af27ddadee879f2b6
Ose Agbadu <osepro@gmail.com>
2021-02-07 13:04:56 -0800

    feat: player turn and logic implemented

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Button from "./Button";
+import PlayerTurn from "./PlayerTurn";
 import "../styles/board.css";
 
-const Board = () => {
-	const items = new Array(9).fill(null);
-	const [player, setPlayer] = useState(true);
-	//const [play, setPlay] = useState();
-	return (
-		<div className="mainBoard">
-			{items.map((item, index) => (
-				<Button key={index} boardItem={index} />
-			))}
-		</div>
-	);
-};
+const items = new Array(9).fill(null);
+
+class Board extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			player: "X",
+		};
+		this.setPlayer = this.setPlayer.bind(this);
+	}
+	setPlayer(e) {
+		if (e.target.value === "") {
+			this.setState((prevState) => ({
+				player: prevState.player === "X" ? "0" : "X",
+			}));
+			e.target.value = this.state.player;
+		}
+	}
+	render() {
+		const { player } = this.state;
+		return (
+			<div className="main">
+				<PlayerTurn playsturn={player} />
+				<div className="mainBoard">
+					{items.map((item, index) => (
+						<Button key={index} play={this.setPlayer} />
+					))}
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Board;
--- src/components/Play.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-class Play extends Component {
-	render() {
-		return <div></div>;
-	}
-}
